223636639b75e0c77d126585be08728a8c568c1c
chenqi <330320307@qq.com>
2019-11-16 11:09:50 +0800

    蒙层组件

---
+++ src/components/Modal/modal/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './index.css'
+
+export default class Modal extends Component {
+    render() {
+        return (
+            <div className="modal" onClick={(e) => {this.fn(e)}}>
+                <div className="modal-content">
+                    <h2>TEST</h2>
+                    <button className="modal-btn" onClick={this.props.hideModal}>隐藏蒙层</button>
+                </div>
+            </div>
+        )
+    }
+    fn = (e) => {
+        if(e.target.className === "modal") {
+            this.props.hideModal();
+        }
+    }
+}
